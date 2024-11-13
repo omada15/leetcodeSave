fd64e73a8cc8b0d074d60327ed87ad01fda00e3c
Omada <137461829+omada15@users.noreply.github.com>
2024-11-12 20:24:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-// add document.write(module)
+let fb = document.createElement("script");
+fb.src = "https://www.gstatic.com/firebasejs/7.2.2/firebase-app.js";
+document.body.appendChild(fb);
+let fbs = document.createElement("script");
+fbs.src = "https://www.gstatic.com/firebasejs/7.2.2/firebase-database.js";
+document.body.appendChild(fbs);
+
+const firebaseConfig = {
+   apiKey: "AIzaSyAWmBlPlhC83etmPAL7XG32yUAuxkj-FWw",
+   authDomain: "leetcode-save.firebaseapp.com",
+   databaseURL: "https://leetcode-save-default-rtdb.firebaseio.com",
+   projectId: "leetcode-save",
+   storageBucket: "leetcode-save.firebasestorage.app",
+   messagingSenderId: "442626282192",
+   appId: "1:442626282192:web:405ed2c031b5ce41e204d5",
+   measurementId: "G-24QQDVHP52"
+};
+firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
 let id = 16216695;
@@ -7,26 +24,23 @@ let prob = prompt("problem #:");
 let lang = prompt("language  (cpp, csharp, python):");
 let _data, _data_sn, _time, _time_sn, send, dbr;
 if (func = 2){
-   try {
-      _data = localStorage.getItem(`${prob}_${id}_${lang}`)
-      _data_sn = `${prob}_${id}_${lang}`
-      _time = localStorage.getItem(`${prob}_${id}_${lang}-updated-time`)
-      _time_sn = `${prob}_${id}_${lang}-updated-time`
+   _data = localStorage.getItem(`${prob}_${id}_${lang}`)
+   _data_sn = `${prob}_${id}_${lang}`
+   _time = localStorage.getItem(`${prob}_${id}_${lang}-updated-time`)
+   _time_sn = `${prob}_${id}_${lang}-updated-time`
 
-      send = {
-         data: _data,
-         data_sn: _data_sn,
-         time: _time,
-         time_sn: _time_sn
-      }
-      dbr = db.ref(`data/${prob}-${lang}`)
-      dbr.set(data).then(() => {
-         console.log('Data sent successfully!');
-      })
-      .catch((error) => {
-         console.error('Error sending data:', error);
-      });
-   } catch {
-      alert("invalid");
+   send = {
+      data: _data,
+      data_sn: _data_sn,
+      time: _time,
+      time_sn: _time_sn
    }
+   dbr = db.ref(`data/${prob}-${lang}`)
+   dbr.set(send).then(() => {
+      console.log('Data sent successfully!');
+   })
+   .catch((error) => {
+      console.error('Error sending data:', error);
+   });
+
 } else if (func = 1) {console.log("ad")}
