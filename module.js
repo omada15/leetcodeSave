f6866f64917ac83df21fbc83b9396decd55824a2
Omada <137461829+omada15@users.noreply.github.com>
2024-11-12 20:59:33 -0500

    Update module.js

--- module.js
+++ module.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAWmBlPlhC83etmPAL7XG32yUAuxkj-FWw",
@@ -10,6 +11,5 @@ const firebaseConfig = {
   appId: "1:442626282192:web:405ed2c031b5ce41e204d5",
   measurementId: "G-24QQDVHP52"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
