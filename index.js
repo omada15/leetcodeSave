aff166cfaa424bdf19608ea48c88307fe95fd381
Omada <137461829+omada15@users.noreply.github.com>
2024-11-15 17:47:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const firebaseConfig = {
 };
 
 var firebase = initializeApp(firebaseConfig);
-
 function objToString (obj) {
     var str = '';
     for (var p in obj) {
@@ -34,21 +33,22 @@ function write(path, data) {
    const db = getDatabase();
    set(ref(db, path), data);
 }
+
 let id = 16216695;
 let _data, func, prob, lang;
+alert("Finished setting up. use ctrl+alt+s to open menu");
 
 document.addEventListener("keydown", function(e) {
    if (e.ctrlKey && e.altKey && e.key === "s") {
-      func = prompt("Which function:\n  [1] get \n  [2] write")
+      func = prompt("Which function:\n  [1] get \n  [2] write");
       prob = prompt("problem #:");
-      lang = prompt("language  (cpp, csharp, python):");
+      lang = prompt("language (cpp, csharp, python):");
       if (func == 2){
          _data = localStorage.getItem(`${prob}_${id}_${lang}`);
-
          write(`data/${prob}-${lang}`, {
             data: _data,
          });
-         alert("sent")
+         alert("sent");
       } else if (func == 1) {
          const dbRef = ref(getDatabase());
          get(child(dbRef, `data/${prob}-${lang}`)).then((snapshot) => {
@@ -57,7 +57,7 @@ document.addEventListener("keydown", function(e) {
                localStorage.setItem(`${prob}_${id}_${lang}`, objToString(snapshot.val()).slice(6))
                localStorage.setItem(`${prob}_${id}_${lang}-update-time`, Date.now());
             } else {
-               alert("No data")
+               alert("No data");
             }
          });
       }
